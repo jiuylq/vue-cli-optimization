c252d4be093e731ff1d6aab5f4c142acb4a99833
jiuylq <jiuylqc@gmail.com>
2020-04-16 23:20:21 +0800

    fix router

--- src/router/index.js
+++ src/router/index.js
@@ -12,29 +12,17 @@ export default new Router({
     //   name: 'Index',
     //   component: () => import('@/components/HelloWorld') // 路由懒加载
     // }
-<<<<<<< HEAD
-=======
-
->>>>>>> common
     // { // 有时候我们想把某个路由下的所有组件都打包在同个异步块 (chunk) 中。只需要使用 命名 chunk，一个特殊的注释语法来提供 chunk name (需要 Webpack > 2.4)
     //   path: '/',
     //   name: 'Index',
     //   component: () => import(/* webpackChunkName: "hello-world" */ '@/components/HelloWorld') // 路由懒加载
     // }
-<<<<<<< HEAD
-=======
-
->>>>>>> common
     // 2、
     {
       path: '/',
       name: 'Index',
       component: resolve => require(['@/components/HelloWorld'], resolve) // 路由懒加载
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> common
     // 3、使用require.ensure 异步引入组件
     // require.ensure(dependencies: String[], callback: function(require), chunkName: String)
     // {
